174da7efe79bce023af119ae646202f4c535ba50
Tyson <tysoncadenhead@gmail.com>
2014-05-06 11:07:01 -0500

    switched facebook comments for disqus

--- js/js/app.js
+++ js/js/app.js
@@ -40,14 +40,6 @@ requirejs([
     'viewModels/posts'
 ], function ($) {
 
-    (function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=1375714595979507";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-
     $(function () {
         $('.disabled a').click(function (e) {
             e.preventDefault();
