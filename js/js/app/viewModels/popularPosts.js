29860edd5e7ba73dd6d480a27b76b8db64d5c62c
Tyson Cadenhead <tysoncadenhead@gmail.com>
2014-11-13 05:56:38 -0600

    Added fallback for popular posts

--- js/js/app/viewModels/popularPosts.js
+++ js/js/app/viewModels/popularPosts.js
@@ -4,6 +4,10 @@ define([
 ], function ($, Handlebars) {
 
   if ($('#popular-posts').length) {
+
+    $('#popular-posts').append('<div class="popular-post"><a href="/blog/unit-testing-sails-js-applications-with-mocha"><img class="span12" src="/images/blog/unit-testing-sails-js-applications-with-mocha.jpg" /><h4>Unit Testing Sails.js Applications With Mocha</h4></a></div>');
+        $('#popular-posts').append('<div class="popular-post"><a href="/blog/cross-domain-post-delete-and-pu"><img class="span12" src="/images/blog/communication.png" /><h4>Cross-Domain Ajax Using POST, DELETE and PUT Methods</h4></a></div>');
+
     $.ajax({
       dataType: 'jsonp',
       url: 'https://disqus.com/api/3.0/threads/listPopular.json',
@@ -24,7 +28,7 @@ define([
             data.response[i].image = $('#image-' + data.response[i].link.split('/')[4]).data('original');
             data.response[i].link = '/blog/' + data.response[i].link.split('/')[4];
           }
-          $('#popular-posts').append('<div class="popular-post"><a href="' + data.response[i].link + '"><img class="span12" src="' + data.response[i].image + '" /><h4>' + data.response[i].title + '</h4></a></div>');
+          $('#popular-posts').html('').append('<div class="popular-post"><a href="' + data.response[i].link + '"><img class="span12" src="' + data.response[i].image + '" /><h4>' + data.response[i].title + '</h4></a></div>');
         }
       }
     });
